2498daacc5feb8bee4de84248a8f7507f5ca0219
alan-are <alanarellano75@outlook.com>
2026-08-17 13:49:47 -0600

    refactor: package emotion detection as EmotionDetection

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,8 @@
+"""EmotionDetection package.
+
+Exposes the emotion_detector function from the emotion_detection module.
+"""
+
+from .emotion_detection import emotion_detector
+
+__all__ = ["emotion_detector"]
