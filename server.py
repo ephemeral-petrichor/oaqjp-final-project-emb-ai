7dd25b0ef91eb40f5bf6e248f67dad5adbed388c
alan-are <alanarellano75@outlook.com>
2026-08-17 14:08:16 -0600

    fix: use Flask(__name__) so templates resolve regardless of working directory

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Emotion Detector")
+app = Flask(__name__)
 
 
 @app.route("/emotionDetector")
